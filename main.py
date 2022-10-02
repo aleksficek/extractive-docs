94552a8e82250c5d1823926a8d2de61d1692fc0b
Aleksander Ficek <37374704+aleksficek@users.noreply.github.com>
2022-10-02 19:40:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from werkzeug.utils import secure_filename
 import io, os
 import PyPDF2
 
-UPLOAD_FOLDER = '/Users/aleksanderficek/software/extractive-docs'
+UPLOAD_FOLDER = ''
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
+# export FLASK_APP=main
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -64,12 +65,12 @@ def upload_file():
 
 @app.route('/pdf_to_text', methods=['GET'])
 def pdf_to_text():
-    pdffileobj=open('2.pdf','rb')
+    pdffileobj=open('neural_ner_acl.pdf','rb')
 
     pdfreader=PyPDF2.PdfFileReader(pdffileobj)
     num_pages=pdfreader.numPages
 
-    file1=open(r"2.txt","a")
+    file1=open(r"neural_ner_acl.txt","a")
 
     for i in range(num_pages):
         print("On pdf page: ", i)
@@ -79,7 +80,30 @@ def pdf_to_text():
 
     return "txt coverted successfully"
 
+@app.route('/call_nemo_llm', methods=['GET'])
+def call_nemo_llm():
+    print("We are in")
 
-
-
+    headers = {
+                "Accept": "application/octet-stream", 
+                "Content-Type": "application/json",
+                "Authorization": "Bearer OW44MDUwc3NxbmU4Y3RhcW5jMm4yNmU0OXA6NWVjNTY4YWItODNlYy00NGFmLWI4N2ItMTE5MDY3ZWJkNTU3"
+                }
+    body = {
+            "prompt": "Once upon a time",
+            "tokens_to_generate": 50,
+            "logprobs": true,
+            "temperature": 1,
+            "top_p": 0,
+            "top_k": 2,
+            "stop": [
+                "\n"
+            ],
+            "random_seed": 0,
+            "repetition_penalty": 1,
+            "beam_search_diversity_rate": 1,
+            "beam_width": 1,
+            "length_penalty": 1
+            }
+    response = request.post(f"http://localhost:9009/completions", headers=headers, json=body)
 
